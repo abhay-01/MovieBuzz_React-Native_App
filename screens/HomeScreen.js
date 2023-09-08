4d1081581fc8ad1a1b66c1ce54f55fbdb1f4bcc6
abhay-01 <pratapsinghabhay999@gmail.com>
2023-09-08 16:29:28 +0530

    HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, Platform,TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar';
+import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline';
+import { ScrollView } from 'react-native-gesture-handler';
 
+
+const ios = Platform.OS == 'ios';
 const HomeScreen = () => {
   return (
-    <View style = {{flex:1}}>
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-      </SafeAreaView >
+    <View className="flex-1 bg-neutral-800">
+    {/* search bar */}
+    <SafeAreaView className={ios? "-mb-2": "mb-3"}>
+      <StatusBar style="light" />
+      <View className="flex-row justify-between items-center mx-4">
+        <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+        <Text 
+          className="text-white text-3xl font-bold">
+            <Text style={{color:'yellow'}}>M</Text>ovies <Text style={{color:'yellow'}}>B</Text>uzz
+        </Text>
+        <TouchableOpacity onPress={()=> navigation.navigate('Search')}>
+          <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+
+      <ScrollView
+      
+      showsVerticalScrollIndicator = {false}
+      contentContainerStyle = {{paddingBottom: 10}}
+      >
+
+      </ScrollView>
+
       </View>
   )
 }
